9ee91868a6a4fc16f4f28c44a140112d5d37fff3
Adam Colton <adamcolton@gmail.com>
2019-01-31 13:14:03 -0500

    adding documentation to Surface

--- shape.go
+++ shape.go
@@ -1,8 +1,10 @@
 package vec2d
 
-// Surface functions describe 2D area parametrically. They should be continuous
-// and not contain any internal creases (such as mapping a circle radially
-// would have).
+// Surface functions describe 2D area parametrically. They should have the
+// following properties
+// * All points on the perimeter should have either t0==0 or t1==0
+// * The surface should have no creases
+// * F(ta0,ta1)==F(tb0,tb1) --> ta0==tb0 && ta1==tb1
 type Surface func(t0, t1 float64) F
 
 type Surfacer interface {
