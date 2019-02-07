15131063e9ae4f4214e4bfa1bbfcfd5135047cc0
Adam Colton <adamcolton@gmail.com>
2019-02-07 10:54:36 -0500

    Cleaning up godoc comments

--- grid/densegrid.go
+++ grid/densegrid.go
@@ -4,11 +4,15 @@ import (
 	"github.com/adamcolton/vec2d"
 )
 
+// DenseGrid is backed by a slice and should have an entry for every node in
+// the grid.
 type DenseGrid struct {
 	Size vec2d.I
 	Data []interface{}
 }
 
+// NewDenseGrid creates a DenseGrid with Data allocated at the correct size. If
+// a Generator is given, the data will be populated.
 func NewDenseGrid(size vec2d.I, generator Generator) *DenseGrid {
 	g := &DenseGrid{
 		Size: size,
@@ -22,14 +26,17 @@ func NewDenseGrid(size vec2d.I, generator Generator) *DenseGrid {
 	return g
 }
 
+// Get a value, fulfills Grid
 func (g *DenseGrid) Get(pt vec2d.I) interface{} {
 	return g.Data[g.Size.Idx(pt.Mod(g.Size))]
 }
 
+// Set a value, fulfills Grid
 func (g *DenseGrid) Set(pt vec2d.I, val interface{}) {
 	g.Data[g.Size.Idx(pt.Mod(g.Size))] = val
 }
 
+// GetSize of the DenseGrid, fulfills Grid
 func (g *DenseGrid) GetSize() vec2d.I {
 	return g.Size
 }
