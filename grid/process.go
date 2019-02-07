cdd03393f9acc99076d5d922745d6858e1743496
Adam Colton <adamcolton@gmail.com>
2019-02-07 12:40:32 -0500

    updating godoc comment

--- grid/process.go
+++ grid/process.go
@@ -4,8 +4,12 @@ import (
 	"github.com/adamcolton/vec2d"
 )
 
+// Processor can perform an operation on a grid by returning a new value for
+// each point
 type Processor func(pt vec2d.I, g Grid) interface{}
 
+// Process takes a grid in and applies the processor to each node and populating
+// the out grid. If no out grid is defined, a DenseGrid is created and returned.
 func Process(in, out Grid, processor Processor) Grid {
 	sz := in.GetSize()
 	if out == nil {
